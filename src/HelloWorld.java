5e49abeb21ae39668dcb85b1f216fdbc9fea237d
Christian Carter <carter.lee2028@gmail.com>
2022-09-27 15:54:21 -0500

    syntax types and variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,11 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.print("Hello, Everyone!");
+        int myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
+        String myString = "seven";
+        System.out.println(myString);
+        char c = myString;
+
     }
 }
