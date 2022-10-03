688e3e174fdf10ed2e5b1b31f607a8d2ae413d78
Christian Carter <carter.lee2028@gmail.com>
2022-10-03 13:52:04 -0500

    methods exercise

---
+++ src/MethodsExercises.java
@@ -0,0 +1,31 @@
+public class MethodsExercises {
+
+    public static void main(String[] args) {
+        multiply(5, 10);
+        division(33, 3);
+        addition(10, 10);
+        subtraction(20, 8);
+        modulus(13, 6);
+
+    }
+    public static void multiply(int a, int b) {
+        System.out.println(a * b);
+
+    }
+
+    public static void division(int x, int y) {
+        System.out.println(x / y);
+    }
+    public static void addition(int c, int d) {
+        System.out.println(c + d);
+    }
+
+    public static void subtraction(int y, int z) {
+        System.out.println(y - z);
+    }
+    public static void modulus(int e, int f) {
+        System.out.println(e % f);
+    }
+
+
+}
